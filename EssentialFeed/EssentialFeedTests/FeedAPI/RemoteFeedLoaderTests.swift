38a576e55531c0b2d6d4a047a703ca55dabf6857
Vetri Thiyagu <vetrithiyagu@192.168.1.4>
2024-07-06 22:39:42 +0530

    Replace 'RemoteFeedLoader.Result' enum with the pre defined 'LoadFeedResult' enum from the Feed module(in preparation to conforms to the 'FeedLoader' protocol)

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -128,13 +128,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWithResult result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWithResult expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+        }
         
         action()
-        
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
     }
     
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
